469551f9ce7f690d095feddfe62d04881675e01f
elvanalandi <elvanalandi@gmail.com>
2024-09-13 16:32:23 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,27 +3,31 @@ const NextJsObfuscatorPlugin= require("nextjs-obfuscator");
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack: (config, { dev }) => {
-      if (!dev) {
-        config.plugins.push(
-          new NextJsObfuscatorPlugin({
-            rotateStringArray: true,
-          })
-        );
-      }
-  
-      // Temporarily disable mini-css-extract-plugin
-      config.module.rules.forEach((rule) => {
-        if (Array.isArray(rule.use)) {
-          rule.use = rule.use.filter((use) => {
-            if (typeof use === 'object' && use.loader && use.loader.includes('mini-css-extract-plugin')) {
-              return false;
-            }
-            return true;
-          });
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+            // Enable filesystem caching for faster rebuilds in production
+            config.cache = {
+              type: 'filesystem', // This ensures builds use a cache stored on disk
+            };
+      
+            // Add the NextJsObfuscatorPlugin to Webpack plugins
+            config.plugins.push(
+              new NextJsObfuscatorPlugin({
+                rotateStringArray: true,
+              })
+            );
+      
+            // Filter out mini-css-extract-plugin from Webpack rules
+            config.module.rules.forEach((rule) => {
+              if (Array.isArray(rule.use)) {
+                rule.use = rule.use.filter((use) => {
+                  // Check if the rule is using mini-css-extract-plugin and remove it
+                  return !(typeof use === 'object' && use.loader && use.loader.includes('mini-css-extract-plugin'));
+                });
+              }
+            });
         }
-      });
-  
+
       return config;
     },
   };
