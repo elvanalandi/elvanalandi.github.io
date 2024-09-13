41fabad60b72e4fc72df884bff133622c14c3714
elvanalandi <elvanalandi@gmail.com>
2024-09-13 16:55:53 +1000

    Update packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/globals.css";
+import "./globals.css";
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 
