6c8376ff0545383cab397690a482cda84281913c
elvanalandi <elvanalandi@gmail.com>
2024-09-13 16:25:35 +1000

    Update packages

--- next.config.js
+++ next.config.js
@@ -2,17 +2,30 @@ const NextJsObfuscatorPlugin= require("nextjs-obfuscator");
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-reactStrictMode: true,
+    reactStrictMode: true,
     webpack: (config, { dev }) => {
-        if (!dev) {
-            config.plugins.push(
-                new NextJsObfuscatorPlugin({
-                    rotateStringArray: true
-            })
+      if (!dev) {
+        config.plugins.push(
+          new NextJsObfuscatorPlugin({
+            rotateStringArray: true,
+          })
         );
+      }
+  
+      // Temporarily disable mini-css-extract-plugin
+      config.module.rules.forEach((rule) => {
+        if (rule.use) {
+          rule.use = rule.use.filter((use) => {
+            if (typeof use === 'object' && use.loader && use.loader.includes('mini-css-extract-plugin')) {
+              return false;
+            }
+            return true;
+          });
         }
-        return config;
-    }
-};
+      });
+  
+      return config;
+    },
+  };
 
 module.exports = nextConfig;
