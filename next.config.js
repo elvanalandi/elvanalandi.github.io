ca0f2a1225ba23ef71c17b3e8700a0e4a6293903
elvanalandi <elvanalandi@gmail.com>
2024-09-13 17:23:04 +1000

    Update packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
+const WebpackObfuscator = require('webpack-obfuscator');
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+        config.plugins.push(
+            new WebpackObfuscator(
+                {
+                    rotateStringArray: true,
+                },
+                [] // Exclude files here if necessary
+            )
+        );
+        }
+        return config;
+    },
 };
 
 module.exports = nextConfig;
